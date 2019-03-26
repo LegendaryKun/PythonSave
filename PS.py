48025c22202dc8c68d08ec2d6d9a9603320463e6
Hu <1324761070@qq.com>
2019-03-26 22:25:31 +0800

    亚马逊图书，已完善！

--- PS.py
+++ PS.py
@@ -1,25 +1,23 @@
 #亚马逊图书信息获取
-
+#在亚马逊上搜索任意一本图书，在基本信息栏找到ASIN，运行脚本输入ASIN，即可获取图书信息
 
 import requests
 from bs4 import BeautifulSoup
 import re
 
-GetUsers_url = 'https://www.amazon.cn/dp/B001BKVXOA/ref\
-=sr_1_1?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&keywords\
-=Core+Python+Applications+Programming&qid=1553266952&s=gateway&sr=8-1'
-#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4094.1 Safari/537.36'}
+#GetUsers_url = 'https://www.amazon.cn/dp/B001BKVXOA'
 
-#html = requests.get(GetUsers_url,headers = headers)
+ASIN = input('请输入亚马逊图书ASIN编号：')  #亚马逊每本图书都有自己的ASIN，通过ASIN即可获取指定图书信息
+GetUsers_url = 'https://www.amazon.cn/dp/'+ASIN
 html = requests.get(GetUsers_url)
 html = BeautifulSoup(html.text, 'html.parser')
 
 
-bookname = html.find('span',class_="a-size-large").text
+bookname = html.find('span',class_="a-size-large").text    #获取图书名
 print('您所查找的亚马逊图书：《%s》\n'%(bookname))
 
 
-soup = html.find_all('noscript')
+soup = html.find_all('noscript')              #获取图书简介
 soup = re.findall('<div>.+?<br/>',str(soup))
 soup = re.split('<div> |<br/>',soup[0])
 soup.pop(0)
@@ -28,7 +26,7 @@ print('《%s》简介：'% bookname)
 print(soup)
 
 
-soup = html.find('div',class_="content")
+soup = html.find('div',class_="content")    #获取图书基本信息
 soup.style.clear()
 print('\n《%s》基本信息：'%bookname)
 for infor in soup.find_all('li'):
