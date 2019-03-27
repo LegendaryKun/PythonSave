d1606d12804886295059b8485ee4def77d361910
Hu <1324761070@qq.com>
2019-03-27 19:23:39 +0800

    亚马逊图书信息，完善图书简介中含有海报无用字

--- PS.py
+++ PS.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-#GetUsers_url = 'https://www.amazon.cn/dp/B001BKVXOA'
 
 ASIN = input('请输入亚马逊图书ASIN编号：')  #亚马逊每本图书都有自己的ASIN，通过ASIN即可获取指定图书信息
 GetUsers_url = 'https://www.amazon.cn/dp/'+ASIN
@@ -18,16 +17,16 @@ print('您所查找的亚马逊图书：《%s》\n'%(bookname))
 
 
 soup = html.find_all('noscript')              #获取图书简介
-soup = re.findall('<div>.+?<br/>',str(soup))
-soup = re.split('<div> |<br/>',soup[0])
-soup.pop(0)
-soup.pop(-1)
-print('《%s》简介：'% bookname)
-print(soup)
+for intro in soup:
+           intro = intro.find('div')  #noscript标签有多个，这里寻找含有div的标签
+           if intro:
+                      print('《%s》简介：'% bookname)
+                      intro = re.split('海报：',intro.text) #简介中去除‘海报：’（有的图书提取的简介信息含有多余‘海报：’）
+                      print(intro[0])                       #有的简介结尾处不含‘海报：’，这里直接选择list中首元素即可。
 
 
 soup = html.find('div',class_="content")    #获取图书基本信息
-soup.style.clear()
+soup.style.clear()                          #style标签中含无用信息
 print('\n《%s》基本信息：'%bookname)
 for infor in soup.find_all('li'):
            print(re.findall('(?m)\w{1}.+',infor.text))
